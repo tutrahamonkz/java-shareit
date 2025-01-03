630373a4aef432529c1e665d641b88068239bc31
tutrahamon <tutrahamonkz@yandex.kz>
2025-01-03 22:47:55 +0300

    add module

--- shareIt-server/src/main/java/ru/practicum/shareit/ShareItServerApp.java
+++ shareIt-server/src/main/java/ru/practicum/shareit/ShareItServerApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItApp {
+public class ShareItServerApp {
 	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
+		SpringApplication.run(ShareItServerApp.class, args);
 	}
 }
